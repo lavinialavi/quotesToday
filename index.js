7888022cc56bf169d2a385d838ae63d893474e39
lavinialavi <laviniutza_90@yahoo.com>
2020-05-28 09:59:11 +0300

    "one"

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ fetch('https://api.kanye.rest')
     })
 
 let catButton = document.getElementById('give-cat')
-
 catButton.addEventListener("click", evt => {
     let catDiv = document.getElementById('cat-pic')
 
@@ -15,7 +14,7 @@ catButton.addEventListener("click", evt => {
         .then(res => res.json())
         .then(cats => {
             cats.forEach(cat => {
-                catDiv.innerHTML = `<h3> The cat wishes you a good day </h3> <img src="${cat.url}" alt="kitty" />`
+                catDiv.innerHTML = `<h3> The cat wishes you a good day </h3> <img src="${cat.url}" alt="kitty"/>`
             });
         })
 })
